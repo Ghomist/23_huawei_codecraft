fbd6290b4d53e110be20eca6ea7ca8c1a56a8761
Ghomist <ghomist@gmail.com>
2023-03-15 01:04:58 +0800

    优化运行脚本

--- compile_and_run.bat
+++ compile_and_run.bat
@@ -15,6 +15,20 @@ cd debug
 .\Robot.exe -f -m .\maps\3.txt -c ..\bin\ "java com.huawei.codecraft.Main" > 3.out
 .\Robot.exe -f -m .\maps\4.txt -c ..\bin\ "java com.huawei.codecraft.Main" > 4.out
 
+type *.out > data.txt
+
+setlocal EnableDelayedExpansion
+set /a sum=0
+
+for /f "tokens=4 delims=,:} " %%a in (data.txt) do (
+    set score=%%a
+    rem set score=!score:~1!
+    set /a sum+=score
+    echo %%a
+)
+
 echo ------------------ >> ..\out.txt
+echo The sum is %sum% >> ..\out.txt
 type *.out >> ..\out.txt
 del *.out
+del data.txt
