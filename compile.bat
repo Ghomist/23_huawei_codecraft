b7bd3fe54a6ab4eee2d63d5a2a2328d04dfe75b9
Ghomist <ghomist@gmail.com>
2023-03-15 12:54:14 +0800

    优化编译脚本

--- compile.bat
+++ compile.bat
@@ -5,6 +5,6 @@ chcp 65001 > nul
 
 dir /b /s /a-d "src\*.java" > sources.txt
 
-javac -d bin/ @sources.txt
+javac -encoding UTF-8 -d bin/ @sources.txt
 
 del sources.txt
--- compile_and_test.bat
+++ compile_and_test.bat
@@ -5,9 +5,9 @@ chcp 65001 > nul
 
 dir /b /s /a-d "src\*.java" > sources.txt
 
-javac -d bin/ @sources.txt
+javac -encoding UTF-8 -d bin/ @sources.txt
 
 del sources.txt
 
 cd debug
-.\Robot_gui.exe -f -m .\maps\2.txt -c ..\bin\ "java com.huawei.codecraft.Main"
+.\Robot_gui.exe -m .\maps\4.txt -c ..\bin\ "java com.huawei.codecraft.Main"
