1e1135ae108a833b5f9ea35f0962121486f3e9d3
Ghomist <ghomist@gmail.com>
2023-03-17 16:52:28 +0800

    增加测试模块

---
+++ src/com/huawei/test/Main.java
@@ -0,0 +1,11 @@
+package com.huawei.test;
+
+import com.huawei.codecraft.math.Line;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Debug...");
+        Line line = new Line();
+        System.out.println(line.point);
+    }
+}
